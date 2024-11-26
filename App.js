cae7022950361d80b7a318e23075413b8ba2aea1
GetaendaleG <GetaendaleG@gmail.com>
2024-11-26 16:15:40 +0300

    add pet components

--- App.js
+++ App.js
@@ -1,7 +1,39 @@
-const App = ()=> {
+const Pet = (props)=>{
     return React.createElement("div",{},
-        React.createElement("h1",{},"Adopt Me")
+        [
+            React.createElement("h1",{},props.name),
+            React.createElement("h2",{},props.animal),
+            React.createElement("h2",{},props.breed)
+        ]
     )
+}
+
+
+
+
+
+const App = ()=> {
+    return React.createElement("div",{},[
+        React.createElement("h1",{},"Adopt Me"),
+        React.createElement(Pet,{
+            animal: "Dog",
+            name: 'Luna',
+            breed: 'Havanese'
+
+        }),
+        React.createElement(Pet,{
+            animal: "Bird",
+            name: 'Pepper',
+            breed: 'Cocktail'
+
+        }),
+        React.createElement(Pet,{
+            animal: "Cat",
+            name: 'Doink',
+            breed: 'Mix'
+
+        })
+    ])
 }
 
 const container = document.getElementById("root")
