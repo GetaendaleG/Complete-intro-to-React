4f8db6c5ad9c6b8a66d57668221a750c80f92e33
GetaendaleG <GetaendaleG@gmail.com>
2024-11-27 17:00:31 +0300

    learned hooks

--- Setup-and-tooling/Adopt-me/src/App.jsx
+++ Setup-and-tooling/Adopt-me/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Pet from "./pet";
+import SearchParams from "./SearchParams";
 
 const App= ()=>{
   return(
     <div>
       <h1>Adopt Me</h1>
-      <Pet animal="Dog" name="Luna" breed="Havanese"/>
-      <Pet animal="Bird" name="Pepper" breed="Cocktail"/>
-      <Pet animal="Cat" name="Doink" breed="Mix"/>
+      <SearchParams />
     </div>
   )
 }
