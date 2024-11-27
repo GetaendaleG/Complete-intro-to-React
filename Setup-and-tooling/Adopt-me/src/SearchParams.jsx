132ac096a66c82bfa8dab902e398d05bccd42504
GetaendaleG <GetaendaleG@gmail.com>
2024-11-27 17:34:46 +0300

    updated serchParams.jsx

--- Setup-and-tooling/Adopt-me/src/SearchParams.jsx
+++ Setup-and-tooling/Adopt-me/src/SearchParams.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-
+const Animals = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = ()=>{
     const [location, setLocation] = useState("");
+    const [animal, setAnimal] = useState("");
     return(
         <div className="search-params">
             <form>
@@ -10,6 +11,15 @@ const SearchParams = ()=>{
                     <input id="location" placeholder="Location" onChange={(e)=> setLocation(e.target.value)} value={location} />
                 </label>
                 <button>Submit</button>
+                <label htmlFor="animal">
+                    Animal
+                    <select id="animal" onChange={e => setAnimal(e.target.value)}>
+                        <option/>
+                        {Animals.map((animal)=>(
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
+                </label>
             </form>
         </div>
     )
